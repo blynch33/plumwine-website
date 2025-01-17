e19e9c357a0ec394bc19899de69dc3d8ec25851e
Brendan Lynch <blynch336@gmail.com>
2025-01-17 12:33:11 -0800

    Updated Vite config and package versions for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom', 'framer-motion']
+  },
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        }
+      }
+    }
+  }
 })
